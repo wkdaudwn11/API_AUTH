56ce47cfb3d5ee5f0f0212477e3eaa2cdf2dfcaa
JangMyungJoo <wkdaudwn11@naver.com>
2019-04-13 11:19:42 +0900

    JsonWebToken

---
+++ config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    JWT_SECRET: 'wkdaudwn' // 내가 아무거나 정하면 됨 키값같은거?
+}
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,16 @@
+const JWT = require('jsonwebtoken');
 const User = require('../models/usersModel');
+const { JWT_SECRET } = require('../config/index');
+
+// 토큰 설정
+signToken = user => {
+    return JWT.sign({
+        iss: 'MyungJoo', // 발행자
+        sub: user.id, // 제목
+        iat: new Date().getTime(), // 현재 시간
+        exp: new Date().setDate(new Date().getDate() + 1) // 유효시간은 24시간 (하루)
+    }, JWT_SECRET);
+}
 
 module.exports = {
     signUp: async (req, res, next) => {
@@ -19,8 +31,11 @@ module.exports = {
         const newUser = new User({ email, password });
         await newUser.save();
 
+        // 토큰 발행
+        const token = signToken(newUser);
+
         // Respond with token
-        res.status(200).json({ user: 'created' });
+        res.status(200).json({ user: token });
     },
 
     signIn: async (req, res, next) => {
