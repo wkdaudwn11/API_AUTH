2569950859ec141d04774d0b0c5df3f72e8e1f60
JangMyungJoo <wkdaudwn11@naver.com>
2019-04-13 11:12:04 +0900

    usersController signUp

--- config/key.js
+++ config/key.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://API_AUTH:1234@cluster0-k8fxr.mongodb.net/test?retryWrites=true'
+    mongoURI: 'mongodb://wkdaudwn11:1q2w3e4r@ds139576.mlab.com:39576/api_auth'
 }
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,26 @@
+const User = require('../models/usersModel');
+
 module.exports = {
     signUp: async (req, res, next) => {
         // Email & Password
         // req.value.body
-        console.log('contents of req.value.body', req.value.body);
-        console.log("usersController signUp()!");
+        // console.log('contents of req.value.body', req.value.body);
+        // console.log("usersController signUp()!");
+
+        const { email, password } = req.value.body;
+
+        //Check if there is a user with the same email
+        const foundUser = await User.findOne({email});
+        if(foundUser) {
+            return res.status(403).json({error: 'Email is already in use'});
+        }
+
+        // Create a new user
+        const newUser = new User({ email, password });
+        await newUser.save();
+
+        // Respond with token
+        res.status(200).json({ user: 'created' });
     },
 
     signIn: async (req, res, next) => {
